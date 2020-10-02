36fe02f79fa3990eb87d8ac4b45c8b9f142ed109
illyanaAvelar <illyavelar012@gmail.com>
2020-10-02 00:25:34 -0300

    Adding inset method

--- SymbolTable.cpp
+++ SymbolTable.cpp
@@ -19,9 +19,27 @@ Line* lines[20];
     }
 
     int insertName(string id, int level, string type){
-        int index = 0;
+        //TODO pegar esse index
+        int i = 0;
         Line* line = new Line(id, level, type, 1);
+
+        if(lines[i] == NULL) {
+            lines[i] = line;
+            //TODO pegar a posição de lines
+            return true;
+        }
+        else{
+            Line* first = lines[i];
+            while(first->next != NULL){
+                first = first->next;
+            }
+            first->next = line;
+            //TODO pegar a posição de lines
+            return true
+        }
+        return false;
     }    
+
 
     bool searchForName(){
         return true;   
