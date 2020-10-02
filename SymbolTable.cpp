9fbe0e5500e0a50714a8ca6862d242158893dee8
illyanaAvelar <illyavelar012@gmail.com>
2020-10-02 00:34:59 -0300

    Adding research method

--- SymbolTable.cpp
+++ SymbolTable.cpp
@@ -41,8 +41,24 @@ Line* lines[20];
     }    
 
 
-    bool searchForName(){
-        return true;   
+    bool searchForName(string id, int level, string type){
+        //TODO pegar esse index (buscando pelo id?)
+        int i = 0;    
+        Line* first = lines[i];
+
+        if(first == NULL){
+            return false;
+        }
+
+        while(first != NULL){
+            if(first->id == id && first->level == level){
+                //retorna a linha, TODO
+                return i;
+            }
+            first = first->next;
+        }
+
+        return false;
     }
 
     string getAttributes(){
@@ -53,5 +69,79 @@ Line* lines[20];
         return true;
     }
 
-
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Function to modify an identi
+// Function to delete an identifier 
+bool SymbolTable::deleteRecord(string id) 
+{ 
+    int index = hashf(id); 
+    Node* tmp = head[index]; 
+    Node* par = head[index]; 
+  
+    // no identifier is present at that index 
+    if (tmp == NULL) { 
+        return false; 
+    } 
+    // only one identifier is present 
+    if (tmp->identifier == id && tmp->next == NULL) { 
+        tmp->next = NULL; 
+        delete tmp; 
+        return true; 
+    } 
+  
+    while (tmp->identifier != id && tmp->next != NULL) { 
+        par = tmp; 
+        tmp = tmp->next; 
+    } 
+    if (tmp->identifier == id && tmp->next != NULL) { 
+        par->next = tmp->next; 
+        tmp->next = NULL; 
+        delete tmp; 
+        return true; 
+    } 
+  
+    // delete at the end 
+    else { 
+        par->next = NULL; 
+        tmp->next = NULL; 
+        delete tmp; 
+        return true; 
+    } 
+    return false; 
+} 
+  
+// Function to find an identifier 
+string SymbolTable::find(string id) 
+{ 
+    int index = hashf(id); 
+    Node* start = head[index]; 
+  
+    if (start == NULL) 
+        return "-1"; 
+  
+    while (start != NULL) { 
+  
+        if (start->identifier == id) { 
+            start->print(); 
+            return start->scope; 
+        } 
+  
+        start = start->next; 
+    } 
+  
+    return "-1"; // not found 
+} 
+  
